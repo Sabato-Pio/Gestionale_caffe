d84482f8e047360fa3ef2e9fafe9ccd7c8dab0bb
Sabato Pio <sianosabatopio@gmail.com>
2026-07-07 10:55:57 +0200

    [IMPLEMENTAZIONE] Modificata struttura in frame
    Aggiunta cassaforte e visualizzazione stato cassa nel frame cassaforte

--- interfaccia.py
+++ interfaccia.py
@@ -13,12 +13,16 @@ def avvia_gui():
     finestra=tk.Tk()
     finestra.title("Gestione caffè di MR.S")
     finestra.geometry("720x400")
+    frame_pagina1 = tk.Frame(finestra)
+    frame_pagina1.pack(fill="both", expand=True)
 
-    testo_benvenuto=tk.Label(finestra,text="Benvenuto nel gestionale di MR.S!")
+    frame_pagina2 = tk.Frame(finestra, bg="grey")
+
+    testo_benvenuto=tk.Label(frame_pagina1,text="Benvenuto nel gestionale di MR.S!")
     testo_benvenuto.pack(pady=20)# aggiunge del margine
 
     # Display soldi e caffè
-    display_saldo = tk.Label(finestra, text="Saldo: 0.00 € | Caffè: 0", font=("Arial", 12))
+    display_saldo = tk.Label(frame_pagina1, text="Saldo: 0.00 € | Caffè: 0", font=("Arial", 12))
     display_saldo.pack(pady=10)
     
     #funzione che indica come funziona il bottone per i caffè
@@ -45,7 +49,7 @@ def avvia_gui():
 
 # CREAZIONE BOTTONE
     bottone_caffe=tk.Button(
-        finestra,
+        frame_pagina1,
         text="+1 Caffè(30cent)",
         command=premi_bottone,
         bg="red",
@@ -57,10 +61,10 @@ def avvia_gui():
     bottone_caffe.pack(pady=20)
 
 # CREAZIONE LABEL INSERIMENTO MANUALE
-    scritta_inserimento=tk.Label(finestra, text="Inserimento manuale soldi", font=("Helvetica",14,"bold"))
+    scritta_inserimento=tk.Label(frame_pagina1, text="Inserimento manuale soldi", font=("Helvetica",14,"bold"))
     scritta_inserimento.pack(pady=8)
 # CREAZIONE INSERIMENTO MANUALE
-    input_importo = tk.Entry(finestra,width=15)
+    input_importo = tk.Entry(frame_pagina1,width=15)
     input_importo.pack()
 # Funzione per prendere l'importo da tastiera
     def invio_importo(event=None):
@@ -75,5 +79,58 @@ def avvia_gui():
 
     input_importo.bind('<Return>', invio_importo) #esegui invio_importo solo se preme invio
 
-    
+    #FUNZIONE PER ANDARE ALLA CASSAFORTE
+    def vai_a_cassaforte():
+    # Nasconde il frame della prima pagina
+        frame_pagina1.pack_forget()
+    # Mostra il frame della seconda pagina
+        frame_pagina2.pack(fill="both", expand=True)
+        bottone_cassaforte.pack_forget
+        bottone_home.pack(pady=20)
+        get_saldo() #Aggiorniamo il saldo della cassaforte
+
+# CREAZIONE BOTTONE CASSAFORTE
+    bottone_cassaforte=tk.Button(
+        frame_pagina1,
+        text="Vai a cassaforte",
+        command=vai_a_cassaforte,
+        bg="red",
+        fg="white",
+        width=13,
+        height=2,
+        font=("Helvetica", 12, "bold")
+        )
+    bottone_cassaforte.pack(pady=20)
+
+    # LABEL SOLDI CASSA E CASSAFORTE
+    display_cassaforte = tk.Label(frame_pagina2, text="Saldo cassa: 0.00 €", font=("Arial", 12),bg="red")
+    display_cassaforte.pack(pady=10)
+
+    #FUNZIONE PER AGGIORNARE SALDO
+    def get_saldo():
+        global stato_cassa #prendiamo lo stato cassa
+    #aggiorniamo le variabili da mostrare
+        soldi=stato_cassa["cassa"] 
+    #mostriamo le variabili
+        display_cassaforte.config(text=f"Saldo:{soldi:.2f}€")
+
+    #FUNZIONE PER LA HOME
+    def vai_alla_home():
+        frame_pagina2.pack_forget()
+        frame_pagina1.pack(fill="both", expand=True)
+        bottone_home.pack_forget()
+        bottone_cassaforte.pack(pady=20)
+    #BOTTONE PER TORNARE ALLA HOME
+    bottone_home=tk.Button(
+        frame_pagina2,
+        text="Torna alla home",
+        command=vai_alla_home,
+        bg="red",
+        fg="white",
+        width=13,
+        height=2,
+        font=("Helvetica",12,"bold")
+        )
+    bottone_home.pack(pady=20)
+
     finestra.mainloop()
